14e20ff78844070ec2f2c15745e42d826d5e8227
princeefue <princeefue@email.com>
2024-04-30 08:41:57 -0400

    Continuous Integration and Continuous Deployment - GitHub Action LINT Error Fix 04

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,12 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS =\
-    ['localhost','https://princeefue-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    ['localhost', 'https://princeefue-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.\
+    cognitiveclass.ai']
 
 CSRF_TRUSTED_ORIGINS =\
-    ['https://princeefue-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    ['https://princeefue-8000.theiadockernext-1-labs-prod-theiak8s-4-\
+    tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
